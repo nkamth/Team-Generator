9a734d2ab4479fe21240312801d9de2cea1bfc71
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-02 19:36:36 -0400

    defined template.js file to generate the entire html page

--- src/template.js
+++ src/template.js
@@ -1,8 +1,15 @@
 function createTeam(team) {
-    console.log(team);
-    
-    function createManager(manager) {
-        return `<div class="card" style="width: 18rem;">
+  console.log(">>>> team is inside create Team:");
+  console.log(team);
+
+  // Create an empty array to push html elements on to and loop through the team data
+  const html = [];
+
+  // Create functions for each type of employee that returns HTML data
+  function createManager(manager) {
+    console.log(">>>> Manager HTML data >>>>");
+
+    let managerHtml = `<div class="card" style="width: 18rem;">
         <div class="card-header">
        ${manager.name} <br/>
        <i class="fas fa-mug-hot"></i>Manager</div>
@@ -12,36 +19,65 @@ function createTeam(team) {
         <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
         </ul>
     </div>`;
+    html.push(managerHtml);
+  }
+
+  function createEngineer(engineer) {
+    console.log(">>>> Engineer HTML data >>>>");
+
+    let engineerHtml = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+       ${engineer.name} <br/>
+       <i class="fas fa-glasses"></i>Engineer</div>
+       <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+        <li class="list-group-item">Github Username:<a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+        </ul>
+    </div>
+    `;
+    html.push(engineerHtml);
+  }
+
+  function createIntern(intern) {
+    console.log(">>> Intern HTML Data");
+
+    let internHtml = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+       ${intern.name} <br/>
+       <i class="fas fa-glasses"></i>Intern</div>
+       <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+        <li class="list-group-item">School:${intern.school}</li>
+        </ul>
+    </div>
+    `;
+    html.push(internHtml);
+  }
+
+  // check for employee type
+  for (let i = 0; i < team.length; i++) {
+    if (team[i].getRole() === "Manager") {
+      createManager(team[i]);
+      console.log(">>>>manager");
     }
-
-    function createEngineer(engineer) {
-
+    if (team[i].getRole() === "Engineer") {
+      createEngineer(team[i]);
     }
-
-    function createIntern(intern) {
-
+    if (team[i].getRole() === "Intern") {
+      createIntern(team[i]);
     }
+  }
 
-    // check for employee type
-    for(let i=0 ; i<team.length;i++){
-        if(team[i].getRole() === 'Manager'){
-            createManager(team[i])
-        }
-        if(team[i].getRole() === 'Engineer'){
-            createEngineer(team[i])
-        }
-        if(team[i].getRole() === 'Intern'){
-            createIntern(team[i])
-        }
-        
-    }
+  // join the HTML blocks
+  return html.join("");
 }
 
-
-
-
 module.exports = (team) => {
-    return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -50,8 +86,11 @@ module.exports = (team) => {
         <title>Team</title>
     </head>
     <body>
-        ${createTeam(team)}
+        <header>
+             <h1> My Team </h1>
+        </header>
+        <main> ${createTeam(team)} </main>
     </body>
     </html>
-    `
-}
+    `;
+};
