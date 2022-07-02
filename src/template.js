183240f893033c9295d636470eeb51aa3a342d48
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-02 19:43:11 -0400

    linked the vanilla css and the fontawesome and bootstrap stylesheets to html

--- src/template.js
+++ src/template.js
@@ -83,7 +83,11 @@ module.exports = (team) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/1e0a13a89f.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../dist/style.css"/>
+        <title>Team Profile generator</title>
     </head>
     <body>
         <header>
