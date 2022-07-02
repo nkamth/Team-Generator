2d88ff6831782556e81d727470c51e8f5cbf1e66
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-02 19:48:21 -0400

    defined css styling in style.css

--- src/template.js
+++ src/template.js
@@ -12,7 +12,7 @@ function createTeam(team) {
     let managerHtml = `<div class="card" style="width: 18rem;">
         <div class="card-header">
        ${manager.name} <br/>
-       <i class="fas fa-mug-hot"></i>Manager</div>
+       <i class="fas fa-mug-hot"></i> Manager</div>
        <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${manager.id}</li>
         <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
@@ -29,7 +29,7 @@ function createTeam(team) {
     <div class="card" style="width: 18rem;">
     <div class="card-header">
        ${engineer.name} <br/>
-       <i class="fas fa-glasses"></i>Engineer</div>
+       <i class="fas fa-glasses"></i> Engineer</div>
        <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${engineer.id}</li>
         <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
@@ -47,7 +47,7 @@ function createTeam(team) {
     <div class="card" style="width: 18rem;">
     <div class="card-header">
        ${intern.name} <br/>
-       <i class="fas fa-glasses"></i>Intern</div>
+       <i class="fas fa-glasses"></i> Intern</div>
        <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${intern.id}</li>
         <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
